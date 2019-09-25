4bbd533014ff9a67b54ee21e30887096073e54ad
Thanos <54344731+rits314@users.noreply.github.com>
2019-09-24 22:04:38 -0700

    Add files via upload

---
+++ Assignment 1 to rishabh .py
@@ -0,0 +1,23 @@
+
+# In[1]:
+
+
+print("hello my name is ritesh and this This is my first coding")
+
+
+# In[2]:
+
+
+print(" %(x)s, %(y)s, %(z)s " %{"x":"value1=5","y":"value2=10","z":"value3=15"})
+
+
+# In[3]:
+
+
+print("Hello! My name is Ritesh\nI am candidate 36\nAnd i am 27yr old\nD+3j")
+
+
+
+
+
+
---
+++ Assignmet 1 to rishabh.py
@@ -0,0 +1,20 @@
+>>> x,y,c,d=10,20,45,67
+>>> x>y
+False
+>>> x<y
+True
+>>> d>y
+True
+>>> c and y > d
+False
+>>> y or c > x
+20
+>>> not x>d
+True
+>>> x or y > c
+10
+>>> x==y
+False
+>>> d < y or x or c
+10
+>>> 
